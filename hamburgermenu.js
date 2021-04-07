3c9f1747df7eac8f26a6ab27f5d7bf1b665b2df4
ayapis <ayapisss@gmail.com>
2021-04-07 22:55:25 +0900

    ナビゲーションメニューにホバーを追加、ヘッダー部分の固定、ダミーのメインコンテンツを追加

--- hamburgermenu.js
+++ hamburgermenu.js
@@ -3,5 +3,6 @@ $(function () {
     $('.p-navi').toggleClass('p-navi-show');
     $('.c-navi__button').toggleClass('c-navi__button-show');
     $('body').toggleClass('body-show');
+    $('.l-header').toggleClass('l-header-show');
   });
 });
