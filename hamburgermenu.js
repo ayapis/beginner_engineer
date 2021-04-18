773ee43a193bf940251003132ca8548abdacc709
takeyama <nonomi0723@yahoo.co.jp>
2021-04-19 00:50:10 +0900

    test

--- hamburgermenu.js
+++ hamburgermenu.js
@@ -1,8 +1,8 @@
-$(function () {
-  $("#header").load("header.html");
-  $('#j-ham').on('click', function () {
-    $('.p-navi').toggleClass('p-navi-show');
-    $('.c-navi__button').toggleClass('c-navi__button-show');
-    $('body').toggleClass('back');
-  });
-});
+// $(function () {
+//   $("#header").load("header.html");
+//   $('#j-ham').on('click', function () {
+//     $('.p-navi').toggleClass('p-navi-show');
+//     $('.c-navi__button').toggleClass('c-navi__button-show');
+//     $('body').toggleClass('back');
+//   });
+// });
