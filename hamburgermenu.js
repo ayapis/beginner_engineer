5bdae2e8c0de535f722e25eb15d60c00369ddc6b
takeyama <nonomi0723@yahoo.co.jp>
2021-04-19 00:44:50 +0900

    動作確認

--- hamburgermenu.js
+++ hamburgermenu.js
@@ -1,4 +1,5 @@
 $(function () {
+  $("#header").load("header.html");
   $('#j-ham').on('click', function () {
     $('.p-navi').toggleClass('p-navi-show');
     $('.c-navi__button').toggleClass('c-navi__button-show');
