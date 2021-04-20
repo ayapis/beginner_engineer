b69bb6a665ae0b617d8e17c327873f7ef70b92c1
ayapis <ayapisss@gmail.com>
2021-04-21 01:22:23 +0900

    Revert "コードが煩雑になっていたため、リファクタリングを行いました。コメントアウトも追加しました。"
    This reverts commit 1cd59f2ee37fa7f6a431b3214db455d218cb9867.

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ $(function () {
     $('#j-ham').on('click', function () {
       $('.p-navi').toggleClass('p-navi-show');
       $('.c-navi__button').toggleClass('c-navi__button-show');
-      $('body').toggleClass('u-back');
+      $('body').toggleClass('back');
     });
   });
   $("#footer").load("footer.html");
